db9b11235ec5fceb2dda763349f4dc84c309f67c
EduSantosBrito <edu.santos.brito@gmail.com>
2023-08-14 19:08:23 +0200

    fix: return output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.100diasdecodigo.dev/",
+  output: 'server',
   integrations: [
     tailwind(),
     image({
